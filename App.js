cb2e3d18afd9dc2962b46d4fe6512fa5b4a700c0
radwa <mail2radwa@gmail.com>
2021-05-10 10:44:00 +0200

    chore: Create store

--- App.js
+++ App.js
@@ -1,7 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { Component } from 'react';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducer from './reducers';
+import middleware from './middleware';
 import { StyleSheet, Text, View } from 'react-native';
 
+// Create store passing it the reducer as first argument 
+// and the middleware function as second argument
+const store = createStore(reducer, middleware);
 
 export default class App extends Component {
   state = {
@@ -10,10 +17,16 @@ export default class App extends Component {
 
   render() {
     return (
-      <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      /*
+   Wraps the app inside the Provider component 
+   passing to it the store 
+  */
+      <Provider store={store}>
+        <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+        </View>
+      </Provider>
     )
   }
 }
